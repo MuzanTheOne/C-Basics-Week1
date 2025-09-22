2d5eec7c0f7f1244e15f2f59e15446680f7f2170
MuzanTheOne <emmanuelnkole458@gmail.com>
2025-09-22 23:59:04 +0200

    Day 1 Hello C program

---
+++ day1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+	printf("My name is Nkole.\n");
+	printf("I am 24 years old.\n");
+	printf("My favourite hobby is gaming.\n");
+
+	char userName[50];
+	int userAge;
+
+	printf("What is your name? ");
+	scanf("%s", userName);
+
+	printf("What is your age? ");
+	scanf("%d", &userAge);
+
+	printf("Hello, %s! You are %d years old.\n", userName, userAge);
+
+	return 0;
+
+}
